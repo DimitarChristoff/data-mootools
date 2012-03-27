bc8131d15fe42c4d16829e47f241a05560181c65
DimitarChristoff <christoff@gmail.com>
2012-03-27 10:16:11 +0100

    refactor for ; police

--- Source/Element/Element.Data.js
+++ Source/Element/Element.Data.js
@@ -11,39 +11,47 @@ license: MIT-style license.
 
 provides: [Element.prototype.data]
 
+requires:
+ - Core/Element
+
 ...
 */
-(function() {
+!function() {
+    "use strict"
 
     var formatDataProperty = function(prop) {
-        return prop.replace('data-', '').camelCase();
-    };
+        return prop.replace('data-', '').camelCase()
+    }
 
     [Document, Element].invoke('implement', {
 
         data: function(property, force) {
-            var data = this.retrieve('dataCollection');
+            var data = this.retrieve('dataCollection'),
+                ii = 0,
+                len,
+                hasData = false,
+                attribs
+
             if (!data || force === true) {
-                data = {};
-                var hasData = false, attribs = !this.propertyIsEnumerable('attributes') ? this.attributes || [] : [];
-                for (var ii = 0, len = attribs.length; ii < len; ++ii) {
+                data = {}
+                attribs = !this.propertyIsEnumerable('attributes') ? this.attributes || [] : []
+                for (len = attribs.length; ii < len; ++ii) {
                     if (attribs[ii].name.indexOf('data-') === 0) {
-                        data[formatDataProperty(attribs[ii].name)] = attribs[ii].value;
-                        hasData = true;
+                        data[formatDataProperty(attribs[ii].name)] = attribs[ii].value
+                        hasData = true
                     }
                 }
 
                 if (!hasData)
-                    data = null;
-                this.store('dataCollection', data);
+                    data = null
+
+                this.store('dataCollection', data)
             }
+
             if (!property)
-                return data;
+                return data
 
-            return data[formatDataProperty(property)] || null;
+            return data[formatDataProperty(property)] || null
         }
-
-    });
-
-
-})();
+    })
+}()
